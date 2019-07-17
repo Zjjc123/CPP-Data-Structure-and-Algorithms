cc7e4e15419bef353c7be9fa2a7da0a0885f6a72
zjjc123 <zjjc123@hotmail.com>
2019-07-16 19:05:31 -0700

    Bubble Sort Fix

--- Algorithms/Sorting/BubbleSort.cpp
+++ Algorithms/Sorting/BubbleSort.cpp
@@ -5,7 +5,7 @@ T* BubbleSort(T* arr, int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = i; j < n - 1; j++)
+        for (int j = 0; j < n - 1; j++)
         {
             if (arr[j] > arr[j+1])
             {
