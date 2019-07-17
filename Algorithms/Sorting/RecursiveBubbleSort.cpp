1b9998eb30e46d558ed582a6c0028ae1d34b4c64
zjjc123 <zjjc123@hotmail.com>
2019-07-16 19:10:23 -0700

    Recursive Bubble Sort

---
+++ Algorithms/Sorting/RecursiveBubbleSort.cpp
@@ -0,0 +1,27 @@
+#include "includes/helper.hpp"
+
+template<typename T>
+T* BubbleSort(T* arr, int n)
+{
+    if (n <= 1)
+    {
+        return arr;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i+1])
+        {
+            arr = Swap(arr, i, i+1);
+        }
+    }
+    BubbleSort(arr, n - 1);
+
+    return arr;
+}
+
+int main()
+{
+    int testArray [10] = {-10, -502, 1, 90, 34, 76, 12, 2, 1000, 0};
+    PrintArray(testArray, 10);
+    PrintArray(BubbleSort(testArray, 10), 10);
+}
